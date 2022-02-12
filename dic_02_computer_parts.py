9c69435fd9fd1c171630c4ac1d13962fe9b09e9b
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-12 13:52:07 +0700

    create list 'chart' to collect selected items add see chart option

--- dic_02_computer_parts.py
+++ dic_02_computer_parts.py
@@ -6,15 +6,22 @@ available_parts = {"1": "computer",
                    "6": "dvd drive",
                    }
 
-
+chart = []
 current_choice = None
 while current_choice != '0':
     if current_choice in available_parts:
         chosen_part = available_parts[current_choice]
         print(f"Adding {chosen_part}")
+        chart.append(available_parts[current_choice])
+    elif current_choice == 'C'.casefold():
+        if not chart:
+            print("Your chart is empty!")
+        else:
+            print(f"In Your chart: {chart}")
     else:
         print("Please Select from: ")
         for key, value in available_parts.items():
             print(f"{key}: {value}")
+        print("C: To See The Chart")
         print("0: EXIT")
     current_choice = input("> ")
