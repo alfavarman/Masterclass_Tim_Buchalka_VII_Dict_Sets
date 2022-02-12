ee00df62c54e1bfd89eb5f8df0dd2a7d2f0801f1
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-12 16:12:02 +0700

    select choice from menu, display selected option

--- dic_meal_planner.py
+++ dic_meal_planner.py
@@ -6,5 +6,13 @@ for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
 
 while True:
-    print("Please Choose From The Menu:"
+    print("Please Choose From The Menu:\n"
           "----------------------------")
+    for key, value in display_dict.items():
+        print(f"{key}: {value}")
+    choice = input(": ")
+
+    if choice == "0":
+        break
+    elif choice in display_dict:
+        print(f"Selected: {display_dict[choice]}")
