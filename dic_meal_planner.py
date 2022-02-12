f1738e0a03e7a9345bf353c17d3ac2d3e9d696f4
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-12 23:56:18 +0700

    new dictrionary to collect ingridient shopping order

--- dic_meal_planner.py
+++ dic_meal_planner.py
@@ -11,24 +11,22 @@ while True:
     for key, value in display_dict.items():
         print(f"{key}: {value}")
     choice = input(": ")
-
+    ingredients_to_buy = {}
     if choice == "0":
         break
     elif choice in display_dict:
         selected_item = display_dict[choice]
-        print(f"Selected: {selected_item}")
         ingredients = recipes[selected_item]
-        print(ingredients)
+        print(f"Selected: {selected_item}\nRequires: {ingredients}\n")
         for ingredient, required_quantity in ingredients.items():
             quantity_in_pantry = pantry.get(ingredient, 0)
-            if required_quantity <= quantity_in_pantry:
-                pass
-                # print(f"{ingredient} in stock")
-            else:
-                quantity_to_order = required_quantity
-                print(f"{ingredient} is not enough, please buy at last: {quantity_to_order}")
-
-
+            if required_quantity >= quantity_in_pantry:
+                ingredients_to_buy[str(ingredient)] = required_quantity
+        print(f"Those ingredients are missing please buy: {ingredients_to_buy}")
+                #pass # print(f"{ingredient} in stock")
+            # else:
+            #     quantity_to_order = required_quantity
+            #     print(f"{ingredient} is not enough, please buy at last: 1{quantity_to_order}")
         # for ingredient in ingredients:
         #     if ingredient not in pantry:
         #         print(f"missing: {ingredient}")
