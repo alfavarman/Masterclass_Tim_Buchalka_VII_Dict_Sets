08c4fed65d486d88d974343dbacfa77793fb5441
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-15 14:46:57 +0700

    task from sets: wtire code to check which preposition are used in quote

--- set_04_tasks_sets.py
+++ set_04_tasks_sets.py
@@ -6,3 +6,11 @@ but the training of the mind to think
 prepositions = {"as", "but", "by", "down", "for", "in", "of", "on", "to", "with"}
 
 # Add your code here.
+# split `text` to list of words
+# intersection of the set of words with preposition set
+# preps_used
+
+preps_used_l = text.split()
+preps_used = set(preps_used_l)
+preps_used.intersection_update(prepositions)
+print(preps_used)
