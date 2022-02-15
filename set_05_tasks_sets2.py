a4dda35c342b0e27288f6ec5d8be7ca078649bfd
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-15 16:09:17 +0700

    my solution

---
+++ set_05_tasks_sets2.py
@@ -0,0 +1,23 @@
+favourites = {'door screen',
+              'frying pan',
+              'roller blind',
+              'football',
+              'coffee grinder',
+              'bush hat',
+              'stirling engine',
+              'cachemira cd',
+              'shirt',
+              }
+
+basket = {'garlic crusher',
+          'stirling engine',
+          'frying pan',
+          'shirt',
+          'bush hat',
+          }
+
+# Add your code here.
+# suggestion - sorted list of favourites - items in basket
+
+suggestion = favourites.difference(basket)
+print(sorted(suggestion))
